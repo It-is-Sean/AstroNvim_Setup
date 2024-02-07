a2af2c9fea456610f42bb837faba69c6a426fdf7
It-is-Sean <jiagnyunchen2006@icloud.com>
2024-02-07 22:15:39 +0800

    Add new functions to wilder.nvim

--- plugins/wider.lua
+++ plugins/wider.lua
@@ -2,6 +2,7 @@ return {
   "gelguy/wilder.nvim",
   lazy = true,
   event = "CmdlineEnter",
-  config = require("plugins.configs.wilder"),
-  dependencies = { "romgrk/fzy-lua-native" }
+  config = require("user.plugins.configs.wilder"),
+  dependencies = { "romgrk/fzy-lua-native" },
+  build = ':UpdateRemotePlugins'
 }
